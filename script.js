85da7d2c95297a4e7148267579ff9df9e1225346
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-22 13:57:50 +0530

    holi spot leader-board image has been changed

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ rulesBtn.addEventListener('click',function (){
 document.querySelector('button').addEventListener('click',function (){
     customeDialog.close();
 })
+
+//crousal code 
+let currentIndex = 0;
+const totalImages = document.querySelectorAll('.crousal-img').length;
+const carousel = document.getElementById('image-carousel');
+
+function showImage(index) {
+    if (index < 0) {
+        currentIndex = totalImages - 1;
+    } else if (index >= totalImages) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+
+    const translateValue = -currentIndex * 100 + '%';
+    carousel.style.transform = 'translateX(' + translateValue + ')';
+}
+
+function prevImage() {
+  console.log('clicked......................');
+    showImage(currentIndex - 1);
+}
+
+function nextImage() {
+  console.log('nextButtoonClicked................................');
+    showImage(currentIndex + 1);
+}
